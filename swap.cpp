bf1fb0b176685ffa93be1fa45149593e16ff46ce
12vishwas <134786056+12vishwas@users.noreply.github.com>
2023-06-22 09:41:02 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void printarray(int arr[],int size){
+
+    for(int i =0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void swapalternate(int arr[],int size){
+
+    for(int i=0; i<size;i+=2){
+        if (i+1<size){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+}
+
+int main(){
+
+    int even[6] = {1,2,3,4,5,6};
+    int odd[5] = {1,2,3,4,5};
+    swapalternate(even,6);
+    printarray(even,6);
+
+    swapalternate(odd,5);
+    printarray(odd,5);
+}
